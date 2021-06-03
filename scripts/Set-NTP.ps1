27bf441550775ef768f0e89b0877cf5b653f014d
D3spere@ux <duykhoa222@gmail.com>
2021-06-03 11:17:35 +0700

    Create Set-NTP.ps1

---
+++ scripts/Set-NTP.ps1
@@ -0,0 +1,14 @@
+﻿# PowerCLI Script for adding ntp to hosts
+# Get Root Directory
+$pwd = $PSScriptRoot
+Invoke-Expression $pwd\Connect-vCenter.ps1
+
+#Set NTP server for all hosts
+Get-VMHost | Add-VMHostNTPServer -NTPserver vn.pool.ntp.org
+
+#Restart NTP services on host
+Get-VMHost | Get-VMHostService | Where-Object {$_.key -eq "ntpd"} | Stop-VMHostService
+Get-VMHost | Get-VMHostService | Where-Object {$_.key -eq "ntpd"} | Start-VMHostService
+
+#Set service to start automatically
+Get-VMHost | Get-VMHostService | Where-Object {$_.key -eq "ntpd"} | Set-VMHostService -policy "on"
