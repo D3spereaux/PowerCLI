3729ef698c78b6deaec6227f27beddc251d1c664
D3spere@ux <duykhoa222@gmail.com>
2021-06-03 11:18:01 +0700

    Create Get-IPAddress.ps1

---
+++ scripts/Get-IPAddress.ps1
@@ -0,0 +1,9 @@
+﻿# Get Root Directory
+$pwd = $PSScriptRoot
+Invoke-Expression $pwd\Connect-vCenter.ps1
+
+#Get list VM PowerOn Usage MHz Realtime
+$starttime = Get-Date –uformat "%I:%M:%S %d-%m-%Y"
+Write-Host "List VMs IP Address has powered on: ($starttime)"
+Get-VM | Where {$_.PowerState -eq "PoweredOn"} | `
+Get-VMGuest | Sort-Object| Format-Table VM, IPAddress
