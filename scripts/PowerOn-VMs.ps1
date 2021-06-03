a45f55dc93064b6eff49bf65efd71b5b044bc6f0
D3spere@ux <duykhoa222@gmail.com>
2021-06-03 11:18:12 +0700

    Create PowerOn-VMs.ps1

---
+++ scripts/PowerOn-VMs.ps1
@@ -0,0 +1,11 @@
+﻿# Get Root Directory
+$pwd = $PSScriptRoot
+Invoke-Expression $pwd\Connect-vCenter.ps1
+
+## Imports CSV file
+$starttime = Get-Date –uformat "%I:%M:%S %d-%m-%Y"
+Write-Host "Powering On the following VMs: ($starttime)"
+Import-Csv "D:\Studying\PowerCLI\scripts\DeployVM.csv" -UseCulture | %{
+## Power On VM
+    Get-VM $_.Name| Where {$_.PowerState -eq "PoweredOff"} | Start-VM -Confirm:$false  | Select-Object Name, PowerState
+}
