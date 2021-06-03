a3d1c5d6d3018c14c2541bd7b4e482e3b0f57455
D3spere@ux <duykhoa222@gmail.com>
2021-06-03 11:18:08 +0700

    Create PowerOff-VMs.ps1

---
+++ scripts/PowerOff-VMs.ps1
@@ -0,0 +1,13 @@
+﻿# Get Root Directory
+$pwd = $PSScriptRoot
+Invoke-Expression $pwd\Connect-vCenter.ps1
+
+## Imports CSV file
+$starttime = Get-Date
+Write-Host "Powering Off the following VMs: ($starttime)"
+Import-Csv "D:\Studying\PowerCLI\scripts\DeployVM.csv" -UseCulture | %{
+## Power Off VM
+    Get-VM $_.Name| Where {$_.PowerState -eq "Suspend"} | Stop-VM -Confirm:$false `
+    | Select-Object Name, PowerState
+    Remove-VM $_.Name -Confirm:$false | Out-Null
+}
