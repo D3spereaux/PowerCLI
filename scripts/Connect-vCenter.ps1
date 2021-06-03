249ec0cd2d542e1be7bce49cc266efb63cdacb4a
D3spere@ux <duykhoa222@gmail.com>
2021-06-03 11:17:46 +0700

    Create Connect-vCenter.ps1

---
+++ scripts/Connect-vCenter.ps1
@@ -0,0 +1,15 @@
+﻿clear-host
+# Get Root Directory
+$pwd = $PSScriptRoot
+# Import the vSphere connect module from the module directory
+Import-Module $pwd\..\modules\vSphereConnect
+# Get a list of servers you are connected to
+$serverlist = $global:DefaultVIServer
+# Check the list of connected servers to the server
+if($serverlist -eq $null) {
+    Write-Host ">> Not connected to server."
+    Write-Host ">> Attempting to connect now..."
+    VSphereConnect
+    }
+Write-Host ">> List Server already connected:" 
+Write-Host "$serverlist" -foreground Green `n
