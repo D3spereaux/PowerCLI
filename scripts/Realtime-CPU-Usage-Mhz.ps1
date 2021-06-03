80f7266e0a9d31d6cd0746553e01ffb893104b20
D3spere@ux <duykhoa222@gmail.com>
2021-06-03 11:18:15 +0700

    Create Realtime-CPU-Usage-Mhz.ps1

---
+++ scripts/Realtime-CPU-Usage-Mhz.ps1
@@ -0,0 +1,10 @@
+﻿# Get Root Directory
+$pwd = $PSScriptRoot
+Invoke-Expression $pwd\Connect-vCenter.ps1
+
+#Get list VM PowerOn Usage MHz Realtime
+$starttime = Get-Date –uformat "%I:%M:%S %d-%m-%Y"
+Write-Host "List VMs usage MHz in realtime: ($starttime)"
+Get-VM | Where {$_.PowerState -eq "PoweredOn"} | Sort-Object |
+Get-Stat -Stat cpu.usagemhz.average -Realtime -MaxSamples 1 | 
+where{$_.Instance -eq ''} | Select @{N='VMs';E={$_.Entity.Name}},Value, Unit
