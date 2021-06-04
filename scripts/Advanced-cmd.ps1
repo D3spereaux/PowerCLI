8e9987d61b5124f4ca6b424cf4f79406f8fd3c15
D3spere@ux <duykhoa222@gmail.com>
2021-06-04 10:54:22 +0700

    Update Advanced-cmd.ps1

--- scripts/Advanced-cmd.ps1
+++ scripts/Advanced-cmd.ps1
@@ -43,7 +43,7 @@ ForEach ($VMHost in $VMHosts)
 }
  
 #Check Lockdown mode status
-Get-VMHost | Select Name, @{Name=&quot;LockdownModeEnabled&quot;;Expression={($_).Extensiondata.Config.adminDisabled}} | ft -auto
+Get-VMHost | Select Name, @{Name="LockdownModeEnabled";Expression={($_).Extensiondata.Config.adminDisabled}} | ft -auto
  
 #Disable Lockdown mode
 Get-VMHost | %{($_ | get-view).ExitLockdownMode()}
@@ -52,25 +52,25 @@ Get-VMHost | %{($_ | get-view).ExitLockdownMode()}
 Get-VMHost | %{($_ | get-view).EnterLockdownMode()}
  
 # Start ESXi SSH Service
-Get-VMHost | Foreach {Start-VMHostService -HostService ($_ | Get-VMHostService | Where { $_.Key -eq &quot;TSM-SSH&quot;} )}
+Get-VMHost | Foreach {Start-VMHostService -HostService ($_ | Get-VMHostService | Where { $_.Key -eq "TSM-SSH"} )}
  
 # Stop ESXi SSH Service
-Get-VMHost | Foreach {Stop-VMHostService -HostService ($_ | Get-VMHostService | Where { $_.Key -eq &quot;TSM-SSH&quot;} ) -Confirm:$false}
+Get-VMHost | Foreach {Stop-VMHostService -HostService ($_ | Get-VMHostService | Where { $_.Key -eq "TSM-SSH"} ) -Confirm:$false}
  
 # View SSH Service state
-Get-VMHost | Get-VMHostService | Where { $_.Key -eq &quot;TSM-SSH&quot; } | Select VMHost, Label, Policy, Running | ft -auto
+Get-VMHost | Get-VMHostService | Where { $_.Key -eq "TSM-SSH" } | Select VMHost, Label, Policy, Running | ft -auto
  
 #Enable SSH Server firewall exception
-Get-VMHost | Get-VMHostFirewallException | Where {$_.Name -eq &quot;SSH Server&quot;} | Set-VMHostFirewallException -Enabled:$true
+Get-VMHost | Get-VMHostFirewallException | Where {$_.Name -eq "SSH Server"} | Set-VMHostFirewallException -Enabled:$true
  
 # Sets remote syslog server
-Get-VMHost | Set-VMHostSysLogServer -SysLogServer &quot;udp://yoursysogserver:514&quot;
+Get-VMHost | Set-VMHostSysLogServer -SysLogServer "udp://yoursysogserver:514"
  
 # Set Hostd logging level to info (default is verbose)
-Get-VMHost | Get-AdvancedSetting -Name Config.HostAgent.log.level | Set-AdvancedSetting -Value &quot;info&quot; -Confirm:$false
+Get-VMHost | Get-AdvancedSetting -Name Config.HostAgent.log.level | Set-AdvancedSetting -Value "info" -Confirm:$false
  
 # Set Vpxa logging level to info (default is verbose)(must be connected to VC)
-Get-VMHost | Get-AdvancedSetting -Name Vpx.Vpxa.config.log.level | Set-AdvancedSetting -Value &quot;info&quot; -Confirm:$false
+Get-VMHost | Get-AdvancedSetting -Name Vpx.Vpxa.config.log.level | Set-AdvancedSetting -Value "info" -Confirm:$false
  
 # Reset syslog service
 $esxcli = Get-EsxCli -VMHost servername
@@ -96,7 +96,7 @@ Get-VMHost MyESXiHost | Get-Log hostd | Select -ExpandProperty Entries | Out-Fil
 Get-VMHost MyESXiHost | Get-Log vpxa | Select -ExpandProperty Entries | Out-File “F:\vpxa.log”
  
 # Get the time on all ESXi hosts
-Get-VMHost | Select Name,@{Name=&quot;Time&quot;;Expression={(get-view $_.ExtensionData.configManager.DateTimeSystem).QueryDateTime()}}
+Get-VMHost | Select Name,@{Name="Time";Expression={(get-view $_.ExtensionData.configManager.DateTimeSystem).QueryDateTime()}}
  
 # Set the time on all ESXi hosts to the PowerCLI host's time
 Get-VMHost | %{(Get-View $_.ExtensionData.configManager.DateTimeSystem).UpdateDateTime((Get-Date -format u)) }
@@ -105,24 +105,24 @@ Get-VMHost | %{(Get-View $_.ExtensionData.configManager.DateTimeSystem).UpdateDa
 Foreach ($esx in Get-VMhost -Location ClusterName | sort Name) { $esx | Get-VMHostStorage -RescanAllHBA -rescanVMFS -refresh }
  
 # Retrieve ntp servers
-Get-VMHost | Select Name, @{N=&quot;NTPServer&quot;;E={$_ | Get-VMHostNtpServer}}, @{N=&quot;ServiceRunning&quot;;E={(Get-VmHostService -VMHost $_ | Where-Object {$_.key -eq &quot;ntpd&quot;}).Running}}
+Get-VMHost | Select Name, @{N="NTPServer";E={$_ | Get-VMHostNtpServer}}, @{N="ServiceRunning";E={(Get-VmHostService -VMHost $_ | Where-Object {$_.key -eq "ntpd"}).Running}}
  
 # Replace ntp servers on hosts
-$oldntpservers = &quot;192.168.0.1&quot;,&quot;192.168.0.2&quot;
-$newntpservers = &quot;192.168.0.20&quot;,&quot;192.168.0.21&quot;
+$oldntpservers = "192.168.0.1","192.168.0.2"
+$newntpservers = "192.168.0.20","192.168.0.21"
 Foreach($vmhost in Get-VMHost){
     #stop ntpd service
-    $vmhost|Get-VMHostService |?{$_.key -eq &quot;ntpd&quot;}|Stop-VMHostService -Confirm:$false
+    $vmhost|Get-VMHostService |?{$_.key -eq "ntpd"}|Stop-VMHostService -Confirm:$false
     #remove ntpservers
     $vmhost|Remove-VMHostNtpServer -NtpServer $oldntpservers -Confirm:$false
     #add new ntpservers
     $vmhost|Add-VmHostNtpServer -NtpServer $newntpservers
     #start ntpd service
-    $vmhost|Get-VMHostService |?{$_.key -eq &quot;ntpd&quot;}|Start-VMHostService
+    $vmhost|Get-VMHostService |?{$_.key -eq "ntpd"}|Start-VMHostService
 }
  
 # Configure ntp service to start and stop with host
-Get-VMHost | Get-VMHostService | ?{$_.key -eq &quot;ntpd&quot;} | Set-VMHostService -Policy &quot;on&quot; -confirm:$false
+Get-VMHost | Get-VMHostService | ?{$_.key -eq "ntpd"} | Set-VMHostService -Policy "on" -confirm:$false
  
 # Change DNS servers, domain name and search suffix
 Get-VMHost | Get-VMHostNetwork | Set-VMHostNetwork -DnsAddress [DNS1 IP address],[DNS2 IP address] -Domain [Domain name] -SearchDomain [Search domain name]
@@ -155,7 +155,7 @@ Move-Inventory -Item (Get-VMHost -Name esxHost) -Destination (Get-Cluster -Name 
 Get-VM | Select Name, @{N=”Cluster”;E={Get-Cluster -VM $_}},@{N=”ESX Host”;E={Get-VMHost -VM $_}},@{N=”Datastore”;E={Get-Datastore -VM $_}}
  
 # Grab powered on Windows VMs in a particular folder
-Get-Folder &quot;projects&quot; | Get-VM | Where-Object {$_.Guest.OSFullName -like &quot;*Windows*&quot; -and $_.PowerState -eq &quot;PoweredOn&quot;} | Sort Name
+Get-Folder "projects" | Get-VM | Where-Object {$_.Guest.OSFullName -like "*Windows*" -and $_.PowerState -eq "PoweredOn"} | Sort Name
  
 # Get VMs with CPU Reservations:
 Get-VM | Get-VMResourceConfiguration | Where {$_.CpuReservationMhz -ne 0} | Select VM,CpuReservationMhz
@@ -164,10 +164,10 @@ Get-VM | Get-VMResourceConfiguration | Where {$_.CpuReservationMhz -ne 0} | Sele
 Get-VM | Get-VMResourceConfiguration | Where {$_.MemReservationMB -ne 0} | Select VM,MemReservationMB
  
 # Reset Memory resource limit to Unlimited
-Get-VM | Get-VMResourceConfiguration | Where-Object {$_.MemLimitMB -ne &quot;-1&quot;} | Set-VMResourceConfiguration -MemLimitMB $null
+Get-VM | Get-VMResourceConfiguration | Where-Object {$_.MemLimitMB -ne "-1"} | Set-VMResourceConfiguration -MemLimitMB $null
  
 # Reset CPU resource limit to Unlimited
-Get-VM | Get-VMResourceConfiguration | Where-Object {$_.CpuLimitMhz -ne &quot;-1&quot;} | Set-VMResourceConfiguration -CPULimitMhz $null
+Get-VM | Get-VMResourceConfiguration | Where-Object {$_.CpuLimitMhz -ne "-1"} | Set-VMResourceConfiguration -CPULimitMhz $null
  
 # Reset both Memory and CPU resource limits to Unlimited (slow)
 Get-VM | Get-VMResourceConfiguration | Set-VMResourceConfiguration -MemLimitMB $null -CpuLimitMhz $null
@@ -182,16 +182,16 @@ Get-VIEvent -maxsamples 10000 | Where {$_.Gettype().Name -eq “VmCreatedEvent�
 Get-VIEvent -maxsamples 10000 | Where {$_.Gettype().Name -eq “VmRemovedEvent”} | Select createdTime, UserName, FullFormattedMessage
  
 # List the last 10 VMs created, cloned or imported
-Get-VIEvent -maxsamples 10000 |where {$_.Gettype().Name-eq &quot;VmCreatedEvent&quot; -or $_.Gettype().Name-eq &quot;VmBeingClonedEvent&quot; -or $_.Gettype().Name-eq &quot;VmBeingDeployedEvent&quot;} |Sort CreatedTime -Descending |Select CreatedTime, UserName,FullformattedMessage -First 10
+Get-VIEvent -maxsamples 10000 |where {$_.Gettype().Name-eq "VmCreatedEvent" -or $_.Gettype().Name-eq "VmBeingClonedEvent" -or $_.Gettype().Name-eq "VmBeingDeployedEvent"} |Sort CreatedTime -Descending |Select CreatedTime, UserName,FullformattedMessage -First 10
  
 # List last 5 VMs removed
-Get-VIEvent -maxsamples 10000 | where {$_.Gettype().Name -eq &quot;VmRemovedEvent&quot;} | Sort CreatedTime -Descending | Select CreatedTime, UserName, FullformattedMessage -First 19
+Get-VIEvent -maxsamples 10000 | where {$_.Gettype().Name -eq "VmRemovedEvent"} | Sort CreatedTime -Descending | Select CreatedTime, UserName, FullformattedMessage -First 19
  
 # List of the VM’s created over the last 14 days
-Get-VIEvent -maxsamples 10000 -Start (Get-Date).AddDays(-14) | where {$_.Gettype().Name-eq &quot;VmCreatedEvent&quot; -or $_.Gettype().Name-eq &quot;VmBeingClonedEvent&quot; -or $_.Gettype().Name-eq &quot;VmBeingDeployedEvent&quot;} |Sort CreatedTime -Descending |Select CreatedTime, UserName,FullformattedMessage
+Get-VIEvent -maxsamples 10000 -Start (Get-Date).AddDays(-14) | where {$_.Gettype().Name-eq "VmCreatedEvent" -or $_.Gettype().Name-eq "VmBeingClonedEvent" -or $_.Gettype().Name-eq "VmBeingDeployedEvent"} |Sort CreatedTime -Descending |Select CreatedTime, UserName,FullformattedMessage
  
 # List of the VMs removed over the last 14 days
-Get-VIEvent -maxsamples 10000 -Start (Get-Date).AddDays(-14) |where {$_.Gettype().Name-eq &quot;VmRemovedEvent&quot;} |Sort CreatedTime -Descending |Select CreatedTime, UserName,FullformattedMessage
+Get-VIEvent -maxsamples 10000 -Start (Get-Date).AddDays(-14) |where {$_.Gettype().Name-eq "VmRemovedEvent"} |Sort CreatedTime -Descending |Select CreatedTime, UserName,FullformattedMessage
  
 # VMs with more than 2 vCPUs:
 Get-VM | Where {$_.NumCPU -gt 2} | Select Name, NumCPU
@@ -225,17 +225,17 @@ Get-VM | Get-Snapshot | Where { $_.Name.Contains(“Consolidate”) } | Remove-S
 Get-VM | Get-Snapshot | Select VM,Name,Description,Created
  
 # List all RDM disks
-Get-VM | Get-HardDisk -DiskType &quot;RawPhysical&quot;,&quot;RawVirtual&quot; | Select Parent,Name,DiskType,ScsiCanonicalName,DeviceName
+Get-VM | Get-HardDisk -DiskType "RawPhysical","RawVirtual" | Select Parent,Name,DiskType,ScsiCanonicalName,DeviceName
  
 # Search datastores for less than x free space
 Get-Datastore | Where-Object {$_.freespaceMB -lt 100000}
-Get-Datastore | Where-Object {$_.freespaceGB -lt 500 -and $_.Name -notlike &quot;*localstorage*&quot;}
+Get-Datastore | Where-Object {$_.freespaceGB -lt 500 -and $_.Name -notlike "*localstorage*"}
  
 # # of VMs per Datastore
-Get-Datastore | Select Name, @{N=&quot;NumVM&quot;;E={@($_ | Get-VM).Count}} | Sort Name
+Get-Datastore | Select Name, @{N="NumVM";E={@($_ | Get-VM).Count}} | Sort Name
  
 # Mount datastore to psdrive
-New-PSDrive -name &quot;mounteddatastore&quot; -Root \ -PSProvider VimDatastore -Datastore (Get-Datastore $datastore)
+New-PSDrive -name "mounteddatastore" -Root \ -PSProvider VimDatastore -Datastore (Get-Datastore $datastore)
  
 # Copy files to mounted datastore
 Copy-Datastoreitem $patchLocation + $patch -Destination mounteddatastore:
@@ -244,17 +244,17 @@ Copy-Datastoreitem $patchLocation + $patch -Destination mounteddatastore:
 del mounteddatastore:$patch
  
 # Unmount datastore from psdrive
-Remove-PSDrive -name &quot;mounteddatastore&quot; -PSProvider VimDatastore
+Remove-PSDrive -name "mounteddatastore" -PSProvider VimDatastore
  
 # Reload inaccessable VMs after a NFS/iSCSI outage
-Get-View -ViewType VirtualMachine | ?{$_.Runtime.ConnectionState -eq &quot;invalid&quot; -or $_.Runtime.ConnectionState -eq &quot;inaccessible&quot;} | %{$_.reload()}
+Get-View -ViewType VirtualMachine | ?{$_.Runtime.ConnectionState -eq "invalid" -or $_.Runtime.ConnectionState -eq "inaccessible"} | %{$_.reload()}
  
 # Get Host HBA WWNs
 Function Get-VMHostHbaWWN {
     param( $VMHost )
     $EsxHostHba = get-vmhosthba -VMHost $VMHost
     foreach( $hba in $EsxHostHba ){
-        $WWN = &quot;{0:x}&quot; -f $hba.PortWorldWideName
+        $WWN = "{0:x}" -f $hba.PortWorldWideName
         $outObj = New-Object PSObject
         $outObj | Add-Member -MemberType NoteProperty -Name Name -Value $VMHost
         $outObj | Add-Member -MemberType NoteProperty -Name WWNDec -Value $hba.PortWorldWideName
@@ -271,7 +271,7 @@ Function Get-VMHostHbaWWN {
  
 # Networking
 # Add Port Group “VLAN 500” with VLAN tag 500 to vSwitch1 on all hosts in a Cluster
-Foreach ($esx in Get-VMHost -Location ClusterName) { $esx | Get-VirtualSwitch -Name vSwitch1 | New-VirtualPortGroup -Name &quot;VLAN500&quot; -VlanId 500 }
+Foreach ($esx in Get-VMHost -Location ClusterName) { $esx | Get-VirtualSwitch -Name vSwitch1 | New-VirtualPortGroup -Name "VLAN500" -VlanId 500 }
  
 # Backup each vNetwork Distributed Switch not including the port groups
 export-vdswitch $switch -Withoutportgroups -Description “Backup of $switch without port groups” -Destination “c:\vSphere\$switch.without_portgroups.$date.zip“
@@ -283,7 +283,7 @@ export-vdswitch $switch -Description “Backup of $switch with port groups” -D
 get-vdswitch $switch | Get-VDPortgroup | foreach { export-vdportgroup -vdportgroup $_ -Description “Backup of port group $($_.name)” -destination “c:\vSphere\$($_.name).portgroup.$date.zip“}
  
 # Swing VMs from one port group to another
-get-vm | get-networkadapter | where-object { $_.networkname -like &quot;OldPortGroup&quot; } | set-networkadapter -networkname &quot;NewPortGroup&quot; -Confirm:$false
+get-vm | get-networkadapter | where-object { $_.networkname -like "OldPortGroup" } | set-networkadapter -networkname "NewPortGroup" -Confirm:$false
  
  
  
@@ -291,10 +291,10 @@ get-vm | get-networkadapter | where-object { $_.networkname -like &quot;OldPortG
 # Gather vCenter Logons
 Get-VIEvent -MaxSamples 100000 | ?{($_ -is [VMware.Vim.UserLoginSessionEvent]) -or ($_ -is [VMware.Vim.UserLogoutSessionEvent])} | %{
 if ($_ -is [VMware.Vim.UserLoginSessionEvent]) {
-  $strLoginOrLogout = &quot;logged in&quot;; $strSourceIP = $_.IpAddress
+  $strLoginOrLogout = "logged in"; $strSourceIP = $_.IpAddress
 }
   else {
-  $strLoginOrLogout = &quot;logged out&quot;; $strSourceIP = $null
+  $strLoginOrLogout = "logged out"; $strSourceIP = $null
   }
 New-Object -TypeName PSObject -Property @{
 UserName = $_.UserName
