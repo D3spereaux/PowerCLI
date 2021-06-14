28a843a0692b313f29fe2e0471f518f7799bba2f
D3spere@ux <duykhoa222@gmail.com>
2021-06-14 14:38:45 +0700

    Update Get-IPAddress.ps1

--- scripts/Get-IPAddress.ps1
+++ scripts/Get-IPAddress.ps1
@@ -5,5 +5,9 @@ Invoke-Expression $pwd\Connect-vCenter.ps1
 #Get list VM PowerOn Usage MHz Realtime
 $starttime = Get-Date –uformat "%I:%M:%S %d-%m-%Y"
 Write-Host "List VMs IP Address has powered on: ($starttime)"
-Get-VM | Where {$_.PowerState -eq "PoweredOn"} | `
-Get-VMGuest | Sort-Object| Format-Table VM, IPAddress
+Get-VM -PipelineVariable vm | where{$_.Guest.Nics.IpAddress} | Get-NetworkAdapter |
+Select @{N='VM';E={$vm.Name}},Name,
+
+   @{N=”IP Address”;E={$nic = $_; ($vm.Guest.Nics | where{$_.Device.Name -eq $nic.Name}).IPAddress -join '|'}},
+
+  MacAddress
